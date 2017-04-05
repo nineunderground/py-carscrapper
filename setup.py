41eef070168805f70c447b1227f23425c1ae2480
inaki rodriguez <nineunderground@gmail.com>
2017-04-05 21:52:18 +0300

    Adding setup.py for dependencies

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='py-carscrapper',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='inaki',
+    author_email='',
+    install_requires=[
+          'beautifulsoup4',
+      ],
+    description=''
+)
